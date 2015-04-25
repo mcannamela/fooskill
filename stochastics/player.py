f136a0d24c81facd428aa352796d3c88a86b0af2
michael <mcannamela>
2015-04-25 18:44:02 -0400

    RF: make Player use base class facilities, endow PlayerAccessor with build_value

--- stochastics/player.py
+++ stochastics/player.py
@@ -1,5 +1,6 @@
 from pymc import Stochastic
-from stochastics.talent_pool import TalentPool, normal_logp
+from stochastics.stochastic_with_log_p_and_random import StochasticWithLogPAndRandom
+from stochastics.talent_pool import TalentPool, compute_gaussian_logp, TalentPoolValueAccessor
 import numpy as np
 
 class PlayerValueAccessor(object):
@@ -20,38 +21,70 @@ class PlayerValueAccessor(object):
     def get_std_dev_of_stopping_power(cls, value):
         return value[3]
 
+    @classmethod
+    def build_value(cls,
+                    mean_firepower,
+                    std_dev_of_firepower,
+                    mean_stopping_power,
+                    std_dev_of_stopping_power):
 
-class Player(Stochastic):
+        return np.array([mean_firepower,
+                         std_dev_of_firepower,
+                         mean_stopping_power,
+                         std_dev_of_stopping_power], dtype=float)
+
+
+class Player(StochasticWithLogPAndRandom):
     
     @classmethod
-    def _player_logp_fun(cls, value, offensive_talent_pool=TalentPool(), defensive_talent_pool=TalentPool()):
+    def _compute_logp(cls, value, offensive_talent_pool_value=None, defensive_talent_pool_value=None):
         mean_firepower = PlayerValueAccessor.get_mean_firepower(value)
         std_dev_of_firepower = PlayerValueAccessor.get_std_dev_of_firepower(value)
-        logp_firepower = offensive_talent_pool.logp_of_skill_parameters(mean_firepower, std_dev_of_firepower)
+        logp_firepower = TalentPool.logp_of_skill_parameters(offensive_talent_pool_value,
+                                                             mean_firepower,
+                                                             std_dev_of_firepower)
 
         mean_stopping_power = PlayerValueAccessor.get_mean_stopping_power(value)
         std_dev_of_stopping_power = PlayerValueAccessor.get_std_dev_of_stopping_power(value)
-        logp_stopping_power = defensive_talent_pool.logp_of_skill_parameters(mean_stopping_power, std_dev_of_stopping_power)
+        logp_stopping_power = TalentPool.logp_of_skill_parameters(defensive_talent_pool_value,
+                                                                  mean_stopping_power,
+                                                                  std_dev_of_stopping_power)
 
         return logp_firepower + logp_stopping_power
 
     @classmethod
+    def _draw_random_sample(cls, offensive_talent_pool_value=None, defensive_talent_pool_value=None):
+        mean_firepower = TalentPool.draw_random_mean_skill(offensive_talent_pool_value)
+        std_dev_of_firepower = TalentPool.draw_random_std_dev_of_skill(offensive_talent_pool_value)
+
+        mean_stopping_power = TalentPool.draw_random_mean_skill(defensive_talent_pool_value)
+        std_dev_of_stopping_power = TalentPool.draw_random_std_dev_of_skill(defensive_talent_pool_value)
+
+        return PlayerValueAccessor.build_value(mean_firepower,
+                                               std_dev_of_firepower,
+                                               mean_stopping_power,
+                                               std_dev_of_stopping_power)
+
+    @classmethod
     def logp_of_firepower(cls, value, firepower):
-        return normal_logp(firepower,
+        return compute_gaussian_logp(firepower,
                            PlayerValueAccessor.get_mean_firepower(value),
                            PlayerValueAccessor.get_std_dev_of_firepower(value))
 
     @classmethod
     def logp_of_stopping_power(cls, value, stopping_power):
-        return normal_logp(stopping_power,
+        return compute_gaussian_logp(stopping_power,
                            PlayerValueAccessor.get_mean_stopping_power(value),
                            PlayerValueAccessor.get_std_dev_of_stopping_power(value))
 
-    def logp_of_performance(self, firepower, stopping_power):
-        return (self.logp_of_firepower(self.value, firepower) +
-                self.logp_of_stopping_power(self.value, stopping_power))
+    @classmethod
+    def logp_of_performance(cls, value, firepower, stopping_power):
+        return (cls.logp_of_firepower(value, firepower) +
+                cls.logp_of_stopping_power(value, stopping_power))
 
-    def __init__(self, *args, **kwargs):
-        kwargs['logp'] = self._player_logp_fun
-        super(Player, self).__init__(*args, **kwargs)
+    def __init__(self, name, offensive_talent_pool, defensive_talent_pool, **kwargs):
+        parents = {'offensive_talent_pool': offensive_talent_pool,
+                   'defensive_talent_pool': defensive_talent_pool}
+        super(Player, self).__init__(name, parents, **kwargs)
+
 
