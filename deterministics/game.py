2453ace7290482af4ad5a83a298e5c4760b0b7a3
michael <mcannamela>
2015-05-06 19:08:29 -0400

    start drafting computation for a game

--- deterministics/game.py
+++ deterministics/game.py
@@ -1,13 +1,24 @@
 from deterministics.deterministic_with_compute_value import DeterministicWithComputeValue
-from stochastics.stochastic_with_log_p_and_random import StochasticWithLogPAndRandom
-from stochastics.talent_pool import TalentPool, compute_gaussian_logp
 import numpy as np
+from deterministics.team import TeamValueAccessor
+
 
 class Game(DeterministicWithComputeValue):
 
     @classmethod
     def _compute_value(cls, team1=None, team2=None):
-        raise NotImplementedError()
+        def get_o_f(t):
+            return TeamValueAccessor.get_offensive_firepower(t)
+
+        def get_s_vs_o(t):
+            return TeamValueAccessor.get_stopping_power_vs_offense(t)
+
+    @classmethod
+    def _compute_probability_of_scoring(cls, firepower, stopping_power):
+        arg = firepower-stopping_power
+        e_arg = np.exp(arg)
+        p = e_arg/(1.0+e_arg)
+        return p
 
 
     def __init__(self, name, team1, team2, **kwargs):
